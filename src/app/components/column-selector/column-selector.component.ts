26dc1c3d4b1649cf97369f80adfb9b5c3a7c3a6d
Erinxon Santana <erinxons@gmail.com>
2024-11-08 10:01:20 -0400

    fix

--- src/app/components/column-selector/column-selector.component.ts
+++ src/app/components/column-selector/column-selector.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import Swal from 'sweetalert2';
 import { SweetalertService } from '../../services/sweetalert.service';
 
 @Component({
--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-loading',
   standalone: true,
-  imports: [],
-  templateUrl: './loading.component.html',
-  styleUrl: './loading.component.css'
+  template: `
+    <div class="fixed inset-0 bg-white bg-opacity-70 flex items-center justify-center z-50">
+      <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-green-600"></div>
+    </div>
+  `
 })
 export class LoadingComponent {
-
 }
